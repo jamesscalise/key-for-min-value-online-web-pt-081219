681b594fdabd1d48668737f45d8fdc242c65aa1c
James Scalise <jamescscalise@gmail.com>
2019-08-20 20:57:57 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,5 +2,14 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-
+  mKey
+  mValue
+  name_hash.each{|key, value|
+    if mKey
+      if value < mValue
+        mKey = key
+        mValue = value
+      end
+    end
+  }
 end
